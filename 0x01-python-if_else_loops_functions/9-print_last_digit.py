668cd88ef168ea0aaa0cdabac58485d285f230d1
Oloyede Olatunde <98672074+Sirtuns@users.noreply.github.com>
2023-05-11 10:46:01 +0100

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,7 +4,7 @@ def print_last_digit(number):
         last_digit = number % 10
     else:
         last_digit = number % -10
-        ld *= -1
+        last_digit *= -1
 
     print("{:d}".format(last_digit), end='')
     return (last_digit)
